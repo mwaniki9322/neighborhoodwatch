ed74df0bd84a1e240b204d648a2b813f4f308f36
Dennis Mwaniki <damos5776@gmail.com>
2021-09-27 18:33:32 +0300

    media setting

--- hoodwatch/urls.py
+++ hoodwatch/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('hood.urls'))
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROO
