eefa3b91366829dedbf959cbec353203eb9ed6a0
Dennis Mwaniki <damos5776@gmail.com>
2021-09-27 10:36:43 +0300

    settings

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -99,7 +99,7 @@ if config('MODE')=="dev":
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
-           'PORT': '',
+           'PORT': '
        }
        
    }
